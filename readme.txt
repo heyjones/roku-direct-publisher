328b5a31d32096c34383699fad11485ea47d7cd8
Chris Jones <chris@>
2017-06-22 14:05:03 -0700

    initial commit

---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== Roku Direct Publisher ===
+Contributors: heyjones
+Tags: roku, direct publisher, video, streaming
+Donate link: https://cash.me/$heyjones
+Requires at least: 3.0
+Tested up to: 4.8
+
+Curate content for Roku through the Direct Publisher program.
+
+== Description ==
+Curate content for Roku through the Direct Publisher program.
+
+== Screenshots ==
+1. Dashboard
+2. Manage Content
+3. Publish
---
+++ roku-direct-publisher.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Roku Direct Publisher
+Plugin URI:  https://github.com/heyjones/roku-direct-publisher
+Description: WordPress Plugin for Roku Direct Publisher (https://developer.roku.com/publish)
+Version:     0.0
+Author:      Chris Jones
+Author URI:  http://heyjones.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
