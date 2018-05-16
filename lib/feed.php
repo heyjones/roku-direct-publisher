bcd52ab47ccbf3de5b7bf7e16273164953fb6dbc
Chris Jones <chris@chris.local>
2018-05-16 12:35:17 -0700

    feed content

--- lib/feed.php
+++ lib/feed.php
@@ -24,7 +24,7 @@ function json(){
   ) );
   $json = array(
     'providerName' => $settings['provider_name'],
-    'lastUpdated' => $videos[0]->post_modified_gmt,
+    'lastUpdated' => date( 'c', strtotime( $videos[0]->post_modified_gmt ) ),
     'language' => $settings['language'],
     'movies' => videos( 'movie' ),
     'series' => videos( 'episode' ),
@@ -50,7 +50,7 @@ function videos( $type ){
     $video = array(
       'id' => $post->ID,
       'title' => $post->post_title,
-      'content' => null,
+      'content' => content( $post ),
       'genres' => array_map( 'strtolower', wp_get_post_terms( $post->ID, 'roku_genre', array( 'fields' => 'names' ) ) ),
       'thumbnail' => get_the_post_thumbnail_url( $post->ID, 'roku_1080' ),
       'releaseDate' => date( 'c', strtotime( $post->post_modified_gmt ) ),
@@ -64,6 +64,42 @@ function videos( $type ){
     $videos[] = $video;
   }
   return $videos;
+}
+
+function content( $post ){
+  //todo: grab this data when the file is uploaded
+  require_once( ABSPATH . 'wp-admin/includes/media.php' );
+  $roku_file = get_field( 'roku_file', $post->ID );
+  $file = get_attached_file( $roku_file['id'], true );
+  $meta = \wp_read_video_metadata( $file );
+  $content = array(
+    'dateAdded' => date( 'c', strtotime( $post->post_date_gmt ) ),
+    'videos' => array(
+      array(
+        'url' => $roku_file['url'],
+        'quality' => quality( $meta['height'] ),
+        'videoType' => strtoupper( $meta['fileformat'] ),
+        'bitrate' => $meta['bitrate']
+      )
+    ),
+    'duration' => $meta['length']
+  );
+  return $content;
+}
+
+function quality( $height ){
+  switch( $height ){
+    case 720:
+    $quality = 'HD – 720p';
+    break;
+    case 1080:
+    $quality = 'FHD – 1080p';
+    break;
+    case 2160:
+    $quality = 'UHD – 4K';
+    break;
+  }
+  return $quality;
 }
 
 function series(){
