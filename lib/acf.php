d0732bf83bab3babe65afeb67a8a7d9e3532c5c8
Chris Jones <chris@heyjones.com>
2018-05-18 13:48:31 -0700

    bug fixes for acf-json paths

--- lib/acf.php
+++ lib/acf.php
@@ -4,14 +4,15 @@ namespace roku_direct_publisher\acf;
 
 if( !class_exists( 'acf' ) ){
   include_once( ROKU_DIRECT_PUBLISHER_DIR . 'vendor/advanced-custom-fields/acf.php' );
+  add_filter( 'acf/settings/dir', __NAMESPACE__ . '\\acf_settings_dir' );
+  add_filter( 'acf/settings/path', __NAMESPACE__ . '\\acf_settings_path' );
 }
 
-add_filter( 'acf/settings/dir', __NAMESPACE__ . '\\acf_settings_dir' );
-add_filter( 'acf/settings/path', __NAMESPACE__ . '\\acf_settings_path' );
 add_filter( 'acf/settings/load_json', __NAMESPACE__ . '\\acf_settings_load_json' );
-// DEV ONLY
-add_filter( 'acf/settings/save_json', __NAMESPACE__ . '\\acf_settings_save_json' );
-// DEV ONLY
+
+if( ROKU_DIRECT_PUBLISHER_DEV ){
+  add_filter( 'acf/settings/save_json', __NAMESPACE__ . '\\acf_settings_save_json' );
+}
 
 function acf_settings_dir( $dir ){
   $dir = ROKU_DIRECT_PUBLISHER_DIR . 'vendor/advanced-custom-fields/';
